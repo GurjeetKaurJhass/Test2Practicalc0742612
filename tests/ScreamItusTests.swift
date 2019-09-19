1fbd31a4689a210e2d113fc3dd87f0ac3be47338
gurjeet <gurjit.babrah@gmail.com>
2019-09-19 11:11:57 -0400

    This is the first commit

---
+++ tests/ScreamItusTests.swift
@@ -0,0 +1,34 @@
+//
+//  ScreamItusTests.swift
+//  ScreamItus-IOS
+//
+//  Created by Gurjeet kaur on 2019-09-19.
+//  Copyright © 2019 room1. All rights reserved.
+//
+
+import XCTest
+
+class ScreamItusTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        
+        
+        
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
